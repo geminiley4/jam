5c35f687740a01b8378996d67d560747cbe94e69
jam <151422183+geminiley4@users.noreply.github.com>
2024-03-03 11:38:29 +0300

    Add files via upload

---
+++ micro2/script.js
@@ -0,0 +1,27 @@
+// SWIPER
+
+const swiper = new Swiper('.swiper', {
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  
+    slidesPerView: 1,
+    spaceBetween: 20,
+    
+    breakpoints: {
+      1450: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 2,
+      }
+    }
+  });
+  
